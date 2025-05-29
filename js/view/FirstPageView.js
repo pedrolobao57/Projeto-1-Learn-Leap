3bfee2970b05f65b8d30fea2890026e432526233
pedrolobao57 <plogneves@gmail.com>
2025-05-30 00:53:01 +0100

    ondas homepage

---
+++ js/view/FirstPageView.js
@@ -0,0 +1,13 @@
+
+let wave1 = document.getElementById("wave1");
+let wave2 = document.getElementById("wave2");
+let wave3 = document.getElementById("wave3");   
+let wave4 = document.getElementById("wave4");   
+
+window.addEventListener("scroll", function() {
+    let value = window.scrollY;
+    wave1.style.backgroundPositionX = 400 + value + "px";
+    wave2.style.backgroundPositionX = 300 + value + "px";
+    wave3.style.backgroundPositionX = 200 + value + "px";
+    wave4.style.backgroundPositionX = 100 + value + "px";
+});
