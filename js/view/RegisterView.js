c9b09fa99f42438da2659d30aea95e013c4f9f5c
pedrolobao57 <plogneves@gmail.com>
2025-06-12 12:58:44 +0100

    login + register base

---
+++ js/view/RegisterView.js
@@ -0,0 +1,60 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+function updateFormSteps() {
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
+  });
+
+  formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressbar() {
+  progressSteps.forEach((progressStep, idx) => {
+    if (idx < formStepsNum + 1) {
+      progressStep.classList.add("progress-step-active");
+    } else {
+      progressStep.classList.remove("progress-step-active");
+    }
+  });
+
+  const progressActive = document.querySelectorAll(".progress-step-active");
+
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
+
+let wave1 = document.getElementById("wave1");
+let wave2 = document.getElementById("wave2");
+let wave3 = document.getElementById("wave3");
+let wave4 = document.getElementById("wave4");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  wave1.style.backgroundPositionX = 400 + value + "px";
+  wave2.style.backgroundPositionX = 300 + value + "px";
+  wave3.style.backgroundPositionX = 200 + value + "px";
+  wave4.style.backgroundPositionX = 100 + value + "px";
+});
--- js/view/SwipeView.js
+++ js/view/SwipeView.js
@@ -18,3 +18,16 @@
     document.querySelectorAll('.decline').forEach(btn =>
       btn.addEventListener('click', () => handleSwipe('decline'))
     );
+
+    let wave1 = document.getElementById("wave1");
+let wave2 = document.getElementById("wave2");
+let wave3 = document.getElementById("wave3");
+let wave4 = document.getElementById("wave4");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  wave1.style.backgroundPositionX = 400 + value + "px";
+  wave2.style.backgroundPositionX = 300 + value + "px";
+  wave3.style.backgroundPositionX = 200 + value + "px";
+  wave4.style.backgroundPositionX = 100 + value + "px";
+});
